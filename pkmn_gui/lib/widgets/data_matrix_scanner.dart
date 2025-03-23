c7276f79e4cf8d6b30a3284a68f807cf49a1ba99
Arne Sahlberg <arne.sahlberg@gmail.com>
2025-03-23 07:30:02 +0100

    Replace ai_barcode_scanner with mobile_scanner for improved scanning functionality and update dependencies. should be faster.

--- pkmn_gui/lib/widgets/data_matrix_scanner.dart
+++ pkmn_gui/lib/widgets/data_matrix_scanner.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart'; // replaced ai_barcode_scanner
 
 class DataMatrixScanner extends StatelessWidget {
   final Function(String) onCodeScanned;
@@ -13,23 +13,48 @@ class DataMatrixScanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        child: AiBarcodeScanner(
-          controller: MobileScannerController(
-            detectionSpeed: DetectionSpeed.unrestricted,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0), //  margin
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Text(
+                    sheetTitle,
+                    style:
+                        Theme.of(
+                          context,
+                        ).textTheme.bodyMedium, // smaller text style
+                    softWrap: true,
+                  ),
+                ),
+              ],
+            ),
           ),
-          sheetTitle: sheetTitle,
-          hideGalleryButton: true,
-          onDetect: (BarcodeCapture capture) {
-            if (capture.barcodes.isNotEmpty) {
-              final barcode = capture.barcodes.first;
-              final scannedValue = barcode.rawValue;
-              if (scannedValue != null) {
-                onCodeScanned(scannedValue);
-              }
-            }
-          },
-        ),
+          Expanded(
+            child: MobileScanner(
+              controller: MobileScannerController(
+                detectionSpeed: DetectionSpeed.unrestricted,
+                detectionTimeoutMs: 100, // Lower timeout may speed up scanning
+                // scanWindow: Rect.fromLTWH(100, 100, 200, 200), // optional: restrict scanning area
+              ),
+              onDetect: (BarcodeCapture capture) {
+                if (capture.barcodes.isNotEmpty) {
+                  final scannedValue = capture.barcodes.first.rawValue;
+                  if (scannedValue != null) {
+                    onCodeScanned(scannedValue);
+                  }
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
